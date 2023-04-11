0e54caa55548f5a01f181e227e4a74c817cbbc1c
tnguyenthuyanh <tnguyenthuyanh@uco.edu>
2023-04-11 14:20:14 -0500

    progressing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ApplicationFormComponent } from './application-form/application-form.component';
+import { UserHomePageComponent } from './user-home-page/user-home-page.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApplicationFormComponent
+    ApplicationFormComponent,
+    UserHomePageComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/employee.info.upload.service.ts
+++ src/app/services/employee.info.upload.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from '../domain/EmployeeService/employee';
 import { UploadResponse } from '../response/upload-response.model';
@@ -12,8 +12,10 @@ export class EmployeeInfoUploadService {
   constructor(private httpClient: HttpClient) { }
 
   uploadEmployeeToDb(emp: Employee): Observable<UploadResponse> {
-    const headers = { 'content-type': 'application/json' }
     const body = JSON.stringify(emp);
+    const headers = new HttpHeaders()
+      .set('Authorization', 'Bearer ' + sessionStorage.getItem('token'))
+      .set('content-type', 'application/json');
     return this.httpClient.post<UploadResponse>('http://localhost:8080/composite/uploadEmpInfo', body, { 'headers': headers });
   }
 }
